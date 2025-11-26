ce60f78668e495f99dfb1d6ef4b26446a803879d
N.zip <184908773+RMA-MUN@users.noreply.github.com>
2025-11-26 21:30:08 +0800

    v-1.5

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 """
 @Author: RMA-MUN
 @email: n3032747608@163.com
-@Date: 2025/9/17
-@Version: 1.4
+@Date: 2025/11/26
+@Version: 1.5
 @Location: https://github.com/RMA-MUN/OnmyoujiAuto
+@Packaging: py2exe-gui
 """
 
-# 打包： py2exe-gui
-
 import sys
-from Onmyoji.core.app_launcher import launch_app
+from OAT.core.app_launcher import launch_app
 
 if __name__ == "__main__":
     sys.exit(launch_app())
