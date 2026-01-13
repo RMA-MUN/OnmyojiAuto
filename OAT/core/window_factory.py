5056172eb691c95fcd097a9e21e3d8ebd17965c5
RMA-MUN <3032747608@qq.com>
2026-01-13 12:22:52 +0800

    增加TypeHints类型注解

--- OAT/core/window_factory.py
+++ OAT/core/window_factory.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-from PyQt6 import QtWidgets
-
-from OAT.tools.mainGui import MainWindow
-
-class WindowFactory:
-    @staticmethod
-    def create_main_window() -> QtWidgets.QMainWindow:
-        """创建并返回主窗口实例"""
-=======
 from PyQt6 import QtWidgets
 
 from OAT.tools.mainGui import MainWindow
@@ -16,5 +6,4 @@ class WindowFactory:
     @staticmethod
     def create_main_window() -> MainWindow:
         """创建并返回主窗口实例"""
->>>>>>> develop
         return MainWindow()
